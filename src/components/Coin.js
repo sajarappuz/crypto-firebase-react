6de3b5d4e1bb9b5d2ce5664e0f985f6ad3404dd0
Sajarappuz <sajarmohammed71@gmail.com>
2023-04-05 18:18:05 +0530

    third commit

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,8 +1,7 @@
 import React from 'react'
-
 function Coin({name,price,icon,symbol}) {
   return (
-    <div>
+    <div className='coins'>
         <h1>name:{name}</h1>
         <img src={icon}/>
         <h3>price:{price}</h3>
